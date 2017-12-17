535bbbf5cb9018eb7d13a24d444476538d9c14a0
Ajesh R Pai <ajeshr2k1@gmail.com>
2017-12-17 14:51:09 +0530

    Initial Commit

---
+++ IOS_Basics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS_Basics
+//
+//  Created by AJESH R PAI on 17/12/17.
+//  Copyright © 2017 Ajesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
